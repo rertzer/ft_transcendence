0a49d7df3a834048eb39f31fa760defdefae31f7
Pierrickjay <pierrick.jay1@gmail.com>
2023-10-05 11:18:12 +0200

    changing docker image, having issue to connect prisma

--- train/src/prisma/prisma.service.ts
+++ train/src/prisma/prisma.service.ts
@@ -8,7 +8,7 @@ export class PrismaService extends PrismaClient {
 		super({
 			datasources: {
 				db: {
-					url: "postgresql://pjay:123@localhost:5434/nest?schema=public"
+					url: "{ dto: {} }postgresql://pjay:123@localhost:5434/nest?schema=public"
 				},
 			},
 		});
