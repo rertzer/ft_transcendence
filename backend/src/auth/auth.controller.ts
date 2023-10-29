3142afee583cf7d8c482be636e8ef225675e1573
rertzer <rertzer@student.42.fr>
2023-10-29 10:35:35 +0100

    uploaded files filtered by size and extension

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -28,24 +28,37 @@ export class AuthController {
     return this.authService.edit(dto);
   }
 
+  
+  
+
   @Post('editAvatar')
   @UseInterceptors(FileInterceptor('file', {
-    storage: diskStorage({
+    limits: { fileSize: 2 * 1024 * 1024 } ,
+    fileFilter: (req, file, cb) => {
+      const allowedFileExtensions = ['.jpg', '.png', '.jpeg', 'xcf', '.pdf'];
+      enum FileValidationErrors {
+      UNSUPPORTED_FILE_TYPE
+  }
+        const extension = extname(file.originalname);
+                 if (allowedFileExtensions.includes(extension)) {
+                    cb(null, true);
+                } else {
+                    // provide the validation error in the request
+                    req.fileValidationError = FileValidationErrors.UNSUPPORTED_FILE_TYPE
+                    cb(null, false);
+                }
+              
+      },
+      storage: diskStorage({
       destination: '/var/avatar',
+      
       filename: (req, file, cb)=>{
         cb(null, `${Date.now()}${extname(file.originalname)}`)
       } 
     })
   }))
   editAvatar(
-    @UploadedFile(
-      new ParseFilePipe({
-        validators: [
-          new FileTypeValidator({ fileType: '.(png|jpeg|jpg|gif)' }),
-          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 4 }),
-        ],
-      }),
-    )
+    @UploadedFile()
     file: Express.Multer.File,
   ) {
     return this.authService.editAvatar(file);
