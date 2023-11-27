077d9bfb34b9a9a613a0ff8912d2fd1c66760c3c
Thibaut GRASSET <tgrasset@made-f0Br8s8.clusters.42paris.fr>
2023-11-27 16:12:59 +0100

    sorting leaderboards starting to work, ready to merge joseph

--- frontend/src/components/Sheets/Data/Data.tsx
+++ frontend/src/components/Sheets/Data/Data.tsx
@@ -64,117 +64,86 @@ function AddLine(sx: number, sy: number, zoom: number, key: number, coordX:numbe
 
 export function Data(props: {sx: number, sy: number, zoom: number}) {
 
-  const [userList, setUserList] = useState<User[]>([{userLogin: "tgrasset",
-    userUsername: "Thib",       
-    userId: 1,        
-    numberGames: 3,         
-    numberGamesBasic: 2,         
-    numberGamesAdvanced: 1,         
-    numberWon: 3,         
-    numberLost: 0,         
-    numberWonBasic: 2,        
-    numberLostBasic: 0,         
-    numberWonAdvanced: 1,         
-    numberLostAdvanced: 0,         
-    totalGameDurationInSec: 350, 
-    totalGameDurationBasicInSec: 200, 
-    totalGameDurationAdvancedInSec: 150,}, 
-    {userLogin: "toto",
-    userUsername: "toto",       
-    userId: 2,        
-    numberGames: 3,         
-    numberGamesBasic: 2,         
-    numberGamesAdvanced: 1,         
-    numberWon: 0,         
-    numberLost: 3,         
-    numberWonBasic: 0,        
-    numberLostBasic: 2,         
-    numberWonAdvanced: 0,         
-    numberLostAdvanced: 1,         
-    totalGameDurationInSec: 350, 
-    totalGameDurationBasicInSec: 200, 
-    totalGameDurationAdvancedInSec: 150,    },
-    {userLogin: "jojo",
-    userUsername: "jojo",       
-    userId: 12353,        
-    numberGames: 341,         
-    numberGamesBasic: 2214,         
-    numberGamesAdvanced: 1142,         
-    numberWon: 241,         
-    numberLost: 34,         
-    numberWonBasic: 235,        
-    numberLostBasic: 2,         
-    numberWonAdvanced: 23523,         
-    numberLostAdvanced: 1,         
-    totalGameDurationInSec: 350, 
-    totalGameDurationBasicInSec: 200, 
-    totalGameDurationAdvancedInSec: 150,    }]);
+  const [userList, setUserList] = useState<User[]>([]);
+  const [listBy, setListBy] = useState("Games Won");
   const auth = useLogin();
 
-  // useEffect(() => {
-	// 	const getUsers = async () => {
-	// 		try {
-	// 			const response = await fetch(`http://${process.env.REACT_APP_URL_MACHINE}:4000/gameStats`, {
-	// 				method: 'GET',
-	// 				headers: { Authorization: auth.getBearer()},
-	// 			});
-	// 			if (!response.ok) {
-	// 				console.error(`Error fetching users: ${response.status}`);
-	// 				return;
-	// 			}
+  useEffect(() => {
+		const getUsers = async () => {
+			try {
+				const response = await fetch(`http://${process.env.REACT_APP_URL_MACHINE}:4000/gameStats`, {
+					method: 'GET',
+					headers: { Authorization: auth.getBearer()},
+				});
+				if (!response.ok) {
+					console.error(`Error fetching users: ${response.status}`);
+					return;
+				}
 
-	// 			const data = await response.json();
-	// 			console.log("data received = ", data);
-	// 			if (!data) {
-	// 				console.log('No list of users');
-	// 			} else {
-	// 				console.log("got list of users", data);
-	// 				setUserList(data);
-	// 			}
-	// 		} catch (error) {
-	// 			console.error('Error fetching user list:', error);
-	// 		}
-	// 	}
-	// 	getUsers();
-	// 	const intervalId = setInterval(getUsers, 5000);
+				const data = await response.json();
+				if (data) {
+					setUserList(data);
+				}
+			} catch (error) {
+				console.error('Error fetching user list:', error);
+			}
+		}
+		getUsers();
+		const intervalId = setInterval(getUsers, 10000);
 
-	// 	return () => clearInterval(intervalId);
-	// }, [])
+		return () => clearInterval(intervalId);
+	}, [])
 
-// function sortUsers(by: string) {
-//     let sorted: User[];
-//     if (by === "Games Won")
-//       sorted = userList.sort((a: User, b: User) => {
-//         if (a.numberWon > b.numberWon)
-// });
-//     else if (by === "Games Played")
-//       sorted = userList.sort((a: User, b: User) => {
-//         return 0;
-//       })
-// }
-
+function sortUsers(users: User[], by: string) {
+    if (by === "Games Won")
+      users.sort((a: User, b: User) => {
+        if (a.numberWon < b.numberWon)
+          return 1;
+        if (a.numberWon > b.numberWon)
+          return -1;
+        return 0;
+      });
+    else if (by === "Games Played")
+      users.sort((a: User, b: User) => {
+        if (a.numberGames < b.numberGames)
+          return 1;
+        if (a.numberGames > b.numberGames)
+          return -1;
+        return 0;
+      });
+    else if (by === "Username")
+      users.sort((a: User, b: User)=>{
+        if (a.userUsername < b.userUsername)
+          return -1;
+        if (a.userUsername > b.userUsername)
+          return 1;
+        return 0;
+      });
+  return (users);
+}
+  console.log("USERLIST", userList);
   return (
     <div key={"contact"}>
       {alternateLine(props.sx, props.sy, props.zoom, userList.length)}
       <CreateStyledCell
       coordX={3} coordY={1} width={1} height={1} scroll_x={props.sx} scroll_y={props.sy} zoom={props.zoom}
       text={"Rank"} fontSize={12} className={"title_data"} />
-      <CreateStyledCell
+      <div  onClick={()=>{setListBy("Username")}}><CreateStyledCell
       coordX={3} coordY={2} width={1} height={1} scroll_x={props.sx} scroll_y={props.sy} zoom={props.zoom}
-      text={"Username"} fontSize={12} className={"title_data"} />
-      <CreateStyledCell
+      text={"Username"} fontSize={12} className={"title_data"} /></div>
+      <div onClick={()=>{setListBy("Games Played")}}><CreateStyledCell
       coordX={3} coordY={3} width={1} height={1} scroll_x={props.sx} scroll_y={props.sy} zoom={props.zoom}
-      text={"Games Played"} fontSize={12} className={"title_data"} />
-      <CreateStyledCell
+      text={"Games Played"} fontSize={12} className={"title_data"} /></div>
+      <div onClick={()=>{setListBy("Games Won")}}><CreateStyledCell
       coordX={3} coordY={4} width={1} height={1} scroll_x={props.sx} scroll_y={props.sy} zoom={props.zoom}
-      text={"Games Won"} fontSize={12} className={"title_data"} />
+      text={"Games Won"} fontSize={12} className={"title_data"} /></div>
       <CreateStyledCell
       coordX={3} coordY={5} width={1} height={1} scroll_x={props.sx} scroll_y={props.sy} zoom={props.zoom}
       text={"Games Lost"} fontSize={12} className={"title_data"} />
       <CreateStyledCell
       coordX={3} coordY={6} width={1} height={1} scroll_x={props.sx} scroll_y={props.sy} zoom={props.zoom}
       text={"Play Time"} fontSize={12} className={"title_data"} />
-      {userList.map((user, index) => {
+      {sortUsers(userList, listBy).map((user, index) => {
         const variableToPass = 4 + index;
         return AddLine(props.sx, props.sy, props.zoom, index + 1, variableToPass, user.userUsername, user.numberGames, user.numberWon, user.numberLost, user.totalGameDurationInSec)
       })}
